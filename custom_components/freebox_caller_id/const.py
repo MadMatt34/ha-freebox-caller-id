4a9e0939ba07f81ef28d31df6ba5abfe8693c77b
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-07-27 11:29:28 +0200

    Create const.py

---
+++ custom_components/freebox_caller_id/const.py
@@ -0,0 +1,15 @@
+"""Constantes pour l'intégration Freebox Caller ID."""
+
+DOMAIN = "freebox_caller_id"
+EVENT_INCOMING_CALL = "freebox_incoming_call"
+
+DEFAULT_HOST = "mafreebox.freebox.fr"
+APP_ID = "fr.ha.callerid"
+APP_NAME = "HA CallerID"
+APP_VERSION = "1.1.0"
+DEVICE_NAME = "Home Assistant"
+
+CONF_HOST = "host"
+CONF_APP_TOKEN = "app_token"
+CONF_SCAN_INTERVAL = "scan_interval"
+DEFAULT_SCAN_INTERVAL = 2
