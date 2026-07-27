c74079523acd0cd348bd4e11c124285630f40468
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-07-27 11:51:04 +0200

    Create sensor.py

---
+++ custom_components/freebox_caller_id/sensor.py
@@ -0,0 +1,41 @@
+"""Capteur affichant le dernier appelant."""
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Configuration du sensor."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities([FreeboxLastCallSensor(coordinator)])
+
+class FreeboxLastCallSensor(CoordinatorEntity, SensorEntity):
+    """Entité stockant les infos du dernier appelant."""
+    
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Dernier appel Freebox"
+        self._attr_unique_id = f"{DOMAIN}_last_call"
+        self._attr_icon = "mdi:phone-log"
+
+    @property
+    def native_value(self):
+        """Affiche le nom ou le numéro de l'appelant."""
+        if self.coordinator.data:
+            name = self.coordinator.data.get("caller_name")
+            num = self.coordinator.data.get("caller_number")
+            return name if name and name != "Inconnu" else num
+        return "Aucun"
+
+    @property
+    def extra_state_attributes(self):
+        """Données détaillées du dernier appel."""
+        if self.coordinator.data:
+            return {
+                "number": self.coordinator.data.get("caller_number"),
+                "name": self.coordinator.data.get("caller_name"),
+                "type": self.coordinator.data.get("call_type"),
+                "duration": self.coordinator.data.get("duration"),
+                "timestamp": self.coordinator.data.get("datetime")
+            }
+        return {}
