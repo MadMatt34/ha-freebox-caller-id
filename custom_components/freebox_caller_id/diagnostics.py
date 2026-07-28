bf7bc1310ef30ca51af437a15d5a0fae17d71164
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-07-28 16:45:12 +0200

    Update diagnostics.py

--- custom_components/freebox_caller_id/diagnostics.py
+++ custom_components/freebox_caller_id/diagnostics.py
@@ -7,7 +7,7 @@ from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import CONF_APP_TOKEN, CONF_HOST, DOMAIN
+from .const import CONF_APP_TOKEN, DOMAIN
 
 # Clés sensibles à masquer (Tokens d'accès + Données personnelles / PII)
 TO_REDACT = {
