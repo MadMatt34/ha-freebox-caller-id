b53dcabf7702fd6b5539ef22c32f54147bbaf535
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-08-21 18:49:19 +0200

    remove import FreeboxCallerIDConfigFlow

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -8,7 +8,6 @@ from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResultType
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.freebox_caller_id.config_flow import FreeboxCallerIDConfigFlow
 from custom_components.freebox_caller_id.const import (
     CONF_APP_TOKEN,
     CONF_HOST,
